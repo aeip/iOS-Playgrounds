0beeb0813c0d3b5ac548f7f229d334c580b4a483
Stasys Meclazcke <aeip@live.com>
2022-01-10 17:31:09 -0600

    Initial Commit

---
+++ iOS Playground/iOS_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PlaygroundApp.swift
+//  iOS Playground
+//
+//  Created by Marcupio on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
